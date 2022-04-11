fcd3f69a91a8600a7eb667611a5816fca2dc75c8
oleksandrportianko <82111498+oleksandrportianko@users.noreply.github.com>
2022-04-11 18:14:05 +0300

    delete change theme

--- src/App.js
+++ src/App.js
@@ -1,49 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import styledComponents from 'styled-components';
 import { ThemeProvider } from 'styled-components';
-import { themeWhite, themeDark } from './ColorsVariables';
+import { themeDark } from './ColorsVariables';
 import './style.css'
-import darkTheme from './Images/dark-theme.svg';
-import whiteTheme from './Images/white-theme.svg';
 
 function App() {
-  const [theme, setTheme] = React.useState(themeWhite);
-
-  useEffect(() => {
-    const currentTheme = localStorage.getItem('theme');
-    if (currentTheme === 'themeWhite') {
-      setTheme(themeWhite);
-    } else if (currentTheme === 'themeDark') {
-      setTheme(themeDark);
-    } else {
-      setTheme(themeWhite);
-      localStorage.setItem('theme', 'themeWhite');
-    }
-  }, [])
-
-  const changeThemeColor = () => {
-    localStorage.setItem('theme', theme.backgroundColor === themeWhite.backgroundColor  ? 'themeDark' : 'themeWhite');
-    const currentTheme =  localStorage.getItem('theme');
-    if (currentTheme === 'themeWhite') {
-      setTheme(themeWhite);
-    }
-    else { 
-      setTheme(themeDark);
-    }
-  }
-
+  const [theme, setTheme] = React.useState(themeDark);
   return (
     <ThemeProvider theme={theme} >
       <Wrapper>
           <Header>
-            <HeaderText>Portfolio</HeaderText>
-            <ChangeColor onClick={() => changeThemeColor()}>
-              {
-                localStorage.getItem('theme') === 'themeDark' ? 
-                <ImgThemeWhite src={whiteTheme} />  : 
-                <ImgThemeDark src={darkTheme} />
-              }
-            </ChangeColor>
           </Header>
           <FirstBlock>
             <FirstBlockText>
@@ -76,25 +42,6 @@ const Header = styledComponents.div`
   align-items: center;
 `;
 
-const HeaderText = styledComponents.div`
-  font-size: 1.3rem;
-  color: ${props => props.theme.colorText};
-  font-weight: bold;
-  padding-left: 10px;
-`
-
-const ChangeColor = styledComponents.button`
-  color: ${props => props.theme.colorText};
-  padding-right: 10px;
-  border: none;
-  background-color: transparent;
-  font-size: 1.125rem;
-  display: flex;
-  &:hover {
-    color: ${props => props.theme.focusTextButton};
-  }
-`
-
 const FirstBlock = styledComponents.div`
   width: 100%;
   height: 200px;
@@ -110,16 +57,4 @@ const FirstBlockText = styledComponents.div`
   text-transform: uppercase;
   font-weight: bold;
   text-align: center;
-`
-
-const ImgThemeDark = styledComponents.img`
-  height: 30px;
-  width: 30px;
-  filter: invert(99%) sepia(39%) saturate(4619%) hue-rotate(168deg) brightness(122%) contrast(104%);
-`
-
-const ImgThemeWhite = styledComponents.img`
-  height: 30px;
-  width: 30px;
-  filter: invert(6%) sepia(19%) saturate(5693%) hue-rotate(208deg) brightness(90%) contrast(104%);;
 `
--- src/ColorsVariables.js
+++ src/ColorsVariables.js
@@ -1,9 +1,3 @@
-export const themeWhite = {
-   backgroundColor: '#D5FFFB',
-   colorText: '#030C24',
-   focusTextButton: '#FF0000',
-}
-
 export const themeDark = {
    backgroundColor: '#030C24',
    colorText: '#D5FFFB',
